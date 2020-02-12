fb673187586fa758c5d8eef2391ad8aeea0111cf
ADMIN <phanquangdtvt94@gmail.com>
2020-02-12 16:56:12 +0700

    fix reponsive for sroll

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+var x = window.matchMedia("(max-width: 700px)");
 /** function scroll  line */
 function scrollingContainer(id) {
-	$('html,body').animate({
+	if (x.matches) {
+		$('html,body').animate({
+	    scrollTop: $(id).offset().top-125},
+	 	'slow'); 
+	} else {
+		$('html,body').animate({
 	    scrollTop: $(id).offset().top-70},
-	 	'slow');     
+	 	'slow'); 
+	}
+	    
 }
